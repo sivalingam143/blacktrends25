71631862ed64f9dd6da44421ea4ca39c5b0ce10c
SANJAY-1508 <psanjaykumar377@gmail.com>
2025-10-23 16:14:25 +0530

    `Update Billing component to use item.id instead of item.billing_id in delete button onClick event.`

--- src/view/listings/Billing.jsx
+++ src/view/listings/Billing.jsx
@@ -237,7 +237,7 @@ const Billing = () => {
           {
             label: "Delete",
             icon: <MdOutlineDelete />,
-            onClick: () => handleDelete(item.billing_id),
+            onClick: () => handleDelete(item.id),
           },
         ]}
         label={<HiOutlineDotsVertical />}
